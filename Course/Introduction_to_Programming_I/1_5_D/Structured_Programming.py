4eed1a06ce376d0d1f027936f6ea4312a5aa49b9
mattya527 <watanabemasaki0527@gmail.com>
2019-12-05 17:00:11 +0900

    Sample

--- Course/Introduction_to_Programming_I/1_5_D/Structured_Programming.py
+++ Course/Introduction_to_Programming_I/1_5_D/Structured_Programming.py
@@ -1,6 +1,15 @@
-n = int(input())
-for i in range(3,n+1):
-    if i % 3 == 0:
-        print("",i,end="")
-    elif i % 10 == 3:
-        print("",i,end="")
+N = int(input())
+x = 0
+
+for i in range(1,N+1):
+
+    if i%3 == 0:
+            print(" %d"%i,end = "");
+    else:
+        x = i
+        while (x):
+            if x%10 == 3:
+                print(" %d"%i,end = "")
+                break
+            x //= 10
+print()
